f4c0675f700f0f6e718d0a91f581257ee89308ca
Hudson Schumaker <hudson.schumaker@hotmail.com>
2017-04-28 02:20:16 -0300

    Frist

---
+++ Assets/CollisionBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+//	void OnCollisionEnter2D(Collision2D o){
+//		if(o.gameObject.CompareTag("Plank")){
+//			Destroy (o.gameObject);
+//		}
+//	}
+
+	void OnCollisionExit2D(Collision2D o){
+		if(o.gameObject.CompareTag("Plank")){
+			Destroy (o.gameObject);
+		}
+	}
+}
---
+++ Assets/Force.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour {
+
+	public float force = 300.5f;
+	public Rigidbody2D bola;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(this.gameObject.CompareTag("Ball")){
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				bola.AddForce (new Vector2 (0, force * Time.deltaTime), ForceMode2D.Impulse);
+				return;
+			}
+		}
+	}
+}
